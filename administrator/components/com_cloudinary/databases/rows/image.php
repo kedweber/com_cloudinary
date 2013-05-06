930ffb5fa10db3b410b0e2026147ea2c178b8a39
Jasper van Rijbroek <jasper199069@live.nl>
2013-05-06 15:55:18 +0200

    Implemented a function to remove the files from cloudinary and the locally cached files.

--- administrator/components/com_cloudinary/databases/rows/image.php
+++ administrator/components/com_cloudinary/databases/rows/image.php
@@ -10,8 +10,99 @@
 
 defined('KOOWA') or die('Protected Resource');
 
+require_once JPATH_LIBRARIES.'/cloudinary/src/Cloudinary.php';
+require_once JPATH_LIBRARIES.'/cloudinary/src/Uploader.php';
+require_once JPATH_LIBRARIES.'/cloudinary/src/Api.php';
+
 class ComCloudinaryDatabaseRowImage extends KDatabaseRowDefault
 {
+	/**
+	 * First we need to select the account to which the image belongs.
+	 *
+	 * This function will first get all the derivatives and request the information,
+	 * this information will be used to remove the cached files (if any)
+	 *
+	 * After this the file is removed from cloudinary and the database.
+	 *
+	 * @return bool result of the delete function.
+	 */
+	public function delete()
+	{
+		// Get the account information
+		$account = $this->getService('com://admin/cloudinary.model.accounts')->id($this->cloudinary_account_id)->getItem();
+
+		// Set the configuration
+		Cloudinary::config(array(
+			'cloud_name' => $account->cloud_name,
+			'api_key' => $account->api_key,
+			'api_secret' => $account->api_secret
+		));
+
+		$api = new \Cloudinary\Api();
+
+		$derivatives = $api->resource($this->public_id)->getArrayCopy()['derived'];
+
+		foreach($derivatives as $derivative)
+		{
+			$data = $this->parseTransformation($derivative['transformation']);
+
+			$dir = JPATH_FILES.DS.'images'.DS.'resized'.DS.$data['height'].DS.$data['width'];
+			if(file_exists($dir.DS.$this->public_id.'.'.$this->format))
+			{
+				KService::get('com://admin/files.controller.file')
+					->container('resized')
+					->folder($data['height'].DS.$data['width'])
+					->name($this->public_id.'.'.$this->format)
+					->delete();
+			}
+		}
+
+		// Remove the image from cloudinary itself.
+		$api->delete_resources(array(
+			$this->public_id
+		));
+
+		// Remove the image from the database.
+		return parent::delete();
+	}
+
+	/**
+	 * This function will parse the transformations string to an array.
+	 *
+	 * @param $transformation string of transformations
+	 * @return array Parsed transformations
+	 */
+	public function parseTransformation($transformation)
+	{
+		$array = array();
+
+		$parts = explode(',', $transformation);
+
+		foreach($parts as $part)
+		{
+			$transformations = explode('_', $part);
+
+			// TODO: This part needs to be extended.
+			switch($transformations[0])
+			{
+				case 'c':
+					$array['crop'] = $transformations[1];
+					break;
+				case 'w':
+					$array['width'] = $transformations[1];
+					break;
+				case 'h':
+					$array['height'] = $transformations[1];
+					break;
+				case 'r':
+					$array['radius'] = $transformations[1];
+					break;
+			}
+		}
+
+		return $array;
+	}
+
 	/**
 	 * getImage will fire some checks and save the file on the filesystem.
 	 * At the end the link to the file is returned.
@@ -24,7 +115,7 @@ class ComCloudinaryDatabaseRowImage extends KDatabaseRowDefault
 	{
 		$parts = explode('/', $url);
 
-		$dir = JPATH_FILES.'/'.'images'.'/'.'resized'.'/'.$data['height'].'/'.$data['width'];
+		$dir = JPATH_FILES.DS.'images'.DS.'resized'.DS.$data['height'].DS.$data['width'];
 		if(!is_dir($dir))
 		{
 			KService::get('com://admin/files.controller.folder')
@@ -35,7 +126,7 @@ class ComCloudinaryDatabaseRowImage extends KDatabaseRowDefault
 				->toArray();
 		}
 
-		if(!file_exists($dir.'/'.end($parts)))
+		if(!file_exists($dir.DS.end($parts)))
 		{
 			KService::get('com://admin/files.controller.file')
 				->container('resized')
@@ -46,7 +137,7 @@ class ComCloudinaryDatabaseRowImage extends KDatabaseRowDefault
 		}
 
 		// @TODO: I need to do something about the names.
-		return 'images/resized'.'/'.$data['height'].'/'.$data['width'].'/'.end($parts);
+		return 'images/resized'.DS.$data['height'].DS.$data['width'].DS.end($parts);
 	}
 
 	public function getContainer()
